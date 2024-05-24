86ee469680dd71a701fae0a0a16fd6e533e44828
danmar669 <141825046+danmar669@users.noreply.github.com>
2024-05-24 21:36:50 +0300

    lesson 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-const message = 'Hello world';
+// console.log(process.argv); -- аргументи запуску
+// console.log(process.env); -- перелік всіх змінних оточення
+// process.exit();
+// console.log(process.cwd());
 
-console.log(message);
+process.on('uncaughtException', (err) => {
+    console.error('SMTH HAPPEND! HELP');
+    process.exit(1);
+});
+
+process.on('warning', (warning) => {
+    console.warn(warning.name);
+    console.warn(warning.message);
+    console.warn(warning.stack);
+})
